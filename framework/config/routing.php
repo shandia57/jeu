275c340a738ae65bbd01090be6b6705f4c11f7e7
EricHoffmann57 <81462264+EricHoffmann57@users.noreply.github.com>
2021-11-28 15:13:19 +0100

    First commit Full MVC: Classes Connection/User Controllers Twig templates

--- framework/config/routing.php
+++ framework/config/routing.php
@@ -1,11 +1,21 @@
 <?php
 
 use App\Controller\Homepage;
+use App\Controller\Logout;
 use App\Controller\Question;
+use App\Controller\UserLogin;
+use App\Controller\Subscribe;
 use Framework\Routing\Route;
+
+
 require_once "../vendor/autoload.php";
 
 return [
     new Route('GET', '/', Homepage::class),
     new Route('GET', '/question/{id}', Question::class),
+    new Route('GET','/connectToGame', UserLogin::class),
+    new Route('POST','/connectToGame', UserLogin::class),
+    new Route('GET','/subscribe', Subscribe::class),
+    new Route('POST','/subscribe', Subscribe::class),
+    new Route('PUT','/connectToGame', Logout::class)
 ];
--- framework/public/index.php
+++ framework/public/index.php
@@ -6,3 +6,4 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 $kernel = new Kernel();
 $kernel->handle($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
 
+
