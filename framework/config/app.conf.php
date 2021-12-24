546432158c38062eaf8485007e35367824d915cf
EricHoffmann57 <81462264+EricHoffmann57@users.noreply.github.com>
2021-12-24 22:03:21 +0100

    Delete app.conf.php

--- framework/config/app.conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'database' => [
-        'connection' => 'mysql:dbname=jeudeloie;host=localhost',
-        'username' => 'root',
-        'password' => '',
-        'charset'  => 'UTF-8'
-    ]
-];
