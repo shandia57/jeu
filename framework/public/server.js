f272dcc77d16749828745669a542bead4fbc22b3
shandia57 <shandia57bim@gmail.com>
2022-01-04 11:56:37 +0100

    chat box created

--- framework/public/server.js
+++ framework/public/server.js
@@ -5,25 +5,37 @@ const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send("../templates/test.html.twig");
+    res.sendFile("../templates/test.html.twig");
 })
 
-http.listen(port, () => {
-    console.log("Server running on port ", port);
-})
 
 
 let players = [];
 
-io.on("connection", client => {
-    client.emit("init", client.id);
+// io.on("connection", client => {
+//     client.emit("init", client.id);
 
-    client.on("players", (player) => {
-        players.push(player);
-        client.emit("players", players);
-        console.log(players);
-    })
+//     client.on("players", (player) => {
+//         players.push(player);
+//         client.emit("players", players);
+//         console.log(players);
+//     })
+// })
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        console.log('coucou: ' + msg);
+        io.emit('chat message', "Zoulette : " + msg);
+    });
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+
+
+http.listen(port, () => {
+    console.log("Server running on port ", port);
 })
-
-// console.log("coucou");
 
